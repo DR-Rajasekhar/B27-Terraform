de17456b6c0eb1706a5aaac5838d825865a8f45d
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 15:09:34 +0530

    created sg.tf and commented outside_vpc.tf

--- outside_vpc.tf
+++ outside_vpc.tf
@@ -1,19 +1,19 @@
-data "aws_vpc" "AWS_Default" {
-  id = "vpc-05fdcc939f017ddfc"
-}
-data "aws_route_table" "AWS_Default-rt" {
-  route_table_id = "rtb-0ec31272a87531841"
-  vpc_id         = data.aws_vpc.AWS_Default.id
-}
-resource "aws_subnet" "default-subnet1-public" {
-  vpc_id            = data.aws_vpc.AWS_Default.id
-  cidr_block        = "10.42.4.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "AWSB27-Public-Subnet-1"
-  }
-}
-resource "aws_route_table_association" "default-subnet1-public" {
-  subnet_id      = aws_subnet.default-subnet1-public.id
-  route_table_id = data.aws_route_table.AWS_Default-rt.id
-}
+# data "aws_vpc" "AWS_Default" {
+#   id = "vpc-05fdcc939f017ddfc"
+# }
+# data "aws_route_table" "AWS_Default-rt" {
+#   route_table_id = "rtb-0ec31272a87531841"
+#   vpc_id         = data.aws_vpc.AWS_Default.id
+# }
+# resource "aws_subnet" "default-subnet1-public" {
+#   vpc_id            = data.aws_vpc.AWS_Default.id
+#   cidr_block        = "10.42.4.0/24"
+#   availability_zone = "us-east-1a"
+#   tags = {
+#     Name = "AWSB27-Public-Subnet-1"
+#   }
+# }
+# resource "aws_route_table_association" "default-subnet1-public" {
+#   subnet_id      = aws_subnet.default-subnet1-public.id
+#   route_table_id = data.aws_route_table.AWS_Default-rt.id
+# }
