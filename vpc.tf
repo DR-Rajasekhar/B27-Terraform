1e03a4ad7c6c80e87db0f78fed9313ad6f3de310
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 11:20:56 +0530

    deleting testvpc03 resource from both state and aws

--- vpc.tf
+++ vpc.tf
@@ -16,15 +16,15 @@ resource "aws_vpc" "testvpc02" {
     environment = "dev"
   }
 }
-resource "aws_vpc" "testvpc03" {
-  cidr_block           = "10.3.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name        = "terraform_testvpc_3"
-    Owner       = "Rajasekhar"
-    environment = "dev"
-  }
-}
+# resource "aws_vpc" "testvpc03" {
+#   cidr_block           = "10.3.0.0/16"
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name        = "terraform_testvpc_3"
+#     Owner       = "Rajasekhar"
+#     environment = "dev"
+#   }
+# }
 # resource "aws_vpc" "testvpc04" {
 #   cidr_block           = "10.4.0.0/16"
 #   enable_dns_hostnames = true
