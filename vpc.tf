f8b1ea6f7d2c40ab8f3e78aafaa5190a582c80bb
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 11:44:29 +0530

    added testvpc02 tf resource in vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,15 +7,15 @@ resource "aws_vpc" "testvpc01" {
     environment = "dev"
   }
 }
-# resource "aws_vpc" "testvpc02" {
-#   cidr_block           = "10.2.0.0/16"
-#   enable_dns_hostnames = true
-#   tags = {
-#     Name        = "terraform_testvpc_2"
-#     Owner       = "Rajasekhar"
-#     environment = "dev"
-#   }
-# }
+resource "aws_vpc" "testvpc02" {
+  cidr_block           = "10.2.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "terraform_testvpc_2"
+    Owner       = "Rajasekhar"
+    environment = "dev"
+  }
+}
 # resource "aws_vpc" "testvpc03" {
 #   cidr_block           = "10.3.0.0/16"
 #   enable_dns_hostnames = true
