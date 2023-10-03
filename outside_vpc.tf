d5405eb38c4107cbf52796e3554e62fe0699e2c8
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 14:57:47 +0530

    modified outside_vpc.tf

--- outside_vpc.tf
+++ outside_vpc.tf
@@ -1,6 +1,10 @@
 data "aws_vpc" "AWS_Default" {
   id = "vpc-05fdcc939f017ddfc"
 }
+data "aws_route_table" "AWS_Default-rt" {
+  route_table_id = "rtb-0ec31272a87531841"
+  vpc_id         = data.aws_vpc.AWS_Default.id
+}
 resource "aws_subnet" "default-subnet1-public" {
   vpc_id            = data.aws_vpc.AWS_Default.id
   cidr_block        = "10.42.4.0/24"
@@ -8,4 +12,8 @@ resource "aws_subnet" "default-subnet1-public" {
   tags = {
     Name = "AWSB27-Public-Subnet-1"
   }
+}
+resource "aws_route_table_association" "default-subnet1-public" {
+  subnet_id      = aws_subnet.default-subnet1-public.id
+  route_table_id = data.aws_route_table.AWS_Default-rt.id
 }
