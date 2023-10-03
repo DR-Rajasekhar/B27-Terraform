019feecb3cb0b3130d920e988d9d33cd04d868ac
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 11:51:13 +0530

    used variables

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
- backend "s3" {
-   bucket = "b27-tfstatefile"
-   key    = "terraform.tfstate"
-   region = "us-east-1"
- }
+  backend "s3" {
+    bucket = "b27-tfstatefile"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,31 @@
+variable "cidr_block" {
+  type        = string
+  default     = "10.1.0.0/16"
+  description = "CIDR block for VPC"
+}
+variable "env" {
+  type        = string
+  default     = "development"
+  description = "environment tag  for VPC"
+}
 resource "aws_vpc" "testvpc01" {
-  cidr_block           = "10.1.0.0/16"
+  cidr_block           = var.cidr_block
   enable_dns_hostnames = true
   tags = {
     Name        = "terraform_testvpc_1"
     Owner       = "Rajasekhar"
-    environment = "dev"
+    environment = var.env
   }
 }
-resource "aws_vpc" "testvpc02" {
-  cidr_block           = "10.2.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name        = "terraform_testvpc_2"
-    Owner       = "Rajasekhar"
-    environment = "dev"
-  }
-}
+# resource "aws_vpc" "testvpc02" {
+#   cidr_block           = "10.2.0.0/16"
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name        = "terraform_testvpc_2"
+#     Owner       = "Rajasekhar"
+#     environment = "dev"
+#   }
+# }
 # resource "aws_vpc" "testvpc03" {
 #   cidr_block           = "10.3.0.0/16"
 #   enable_dns_hostnames = true
