66aa759320022471f7a921cc47c1adf54310220f
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 10:46:15 +0530

    kept tf resource name unique in vpc.tf file

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
-provider "aws" {   
-   region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 terraform {
- required_version = "1.5.7" #Forcing version of Terraform needs to be used
- required_providers {
-   aws = {
-     version = "5.19.0" #Forcing version of the plugin needs to be used.
-     source = "hashicorp/aws"
-   }
- }
+  required_version = "1.5.7" #Forcing version of Terraform needs to be used
+  required_providers {
+    aws = {
+      version = "5.19.0" #Forcing version of the plugin needs to be used.
+      source  = "hashicorp/aws"
+    }
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,27 +1,36 @@
-resource "aws_vpc" "default" {
-   cidr_block = "10.1.0.0/16"
-   enable_dns_hostnames = true
-   tags = {
-   Name = "terraform_testvpc_1"
-   Owner = "Rajasekhar"
-   environment = "dev"
-   }
+resource "aws_vpc" "testvpc1" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "terraform_testvpc_1"
+    Owner       = "Rajasekhar"
+    environment = "dev"
+  }
 }
-resource "aws_vpc" "default" {
-   cidr_block = "10.2.0.0/16"
-   enable_dns_hostnames = true
-   tags = {
-   Name = "terraform_testvpc_2"
-   Owner = "Rajasekhar"
-   environment = "dev"
-   }
+resource "aws_vpc" "testvpc2" {
+  cidr_block           = "10.2.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "terraform_testvpc_2"
+    Owner       = "Rajasekhar"
+    environment = "dev"
+  }
 }
-resource "aws_vpc" "default" {
-   cidr_block = "10.3.0.0/16"
-   enable_dns_hostnames = true
-   tags = {
-   Name = "terraform_testvpc_3"
-   Owner = "Rajasekhar"
-   environment = "dev"
-   }
+resource "aws_vpc" "testvpc3" {
+  cidr_block           = "10.3.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "terraform_testvpc_3"
+    Owner       = "Rajasekhar"
+    environment = "dev"
+  }
+}
+resource "aws_vpc" "testvpc4" {
+  cidr_block           = "10.4.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "terraform_testvpc_4"
+    Owner       = "Rajasekhar"
+    environment = "dev"
+  }
 }
