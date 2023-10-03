f0e85faa65fccb09be9c26de27c282531dc90923
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-03 15:30:54 +0530

    kept sg in list

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web-1" {
   instance_type               = var.instance_type
   key_name                    = var.key
   subnet_id                   = data.terraform_remote_state.devopsb27-state.outputs.vpc_subnet1
-  vpc_security_group_ids      = data.terraform_remote_state.devopsb27-state.outputs.sg_id
+  vpc_security_group_ids      = [ data.terraform_remote_state.devopsb27-state.outputs.sg_id ]
   associate_public_ip_address = true
   tags = {
     Name       = "Test-Server-1"
