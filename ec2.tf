4cf125da85dc9c87e445d4f90975a21474054c0f
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-10-04 10:56:35 +0530

    modified vpc-flowlogs.tf file

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-1" {
   ami                         = var.imagename
   instance_type               = "t2.nano"
-  key_name                    = "ohio"
+  key_name                    = "ofcDesktop"
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
--- vpc.tf
+++ vpc.tf
@@ -6,11 +6,8 @@ resource "aws_vpc" "testvpc01" {
     Owner       = "Rajasekhar"
     environment = var.env
   }
-  depends_on = [
-    aws_s3_bucket.vpcflowlogsbucket
-  ]
 }
-resource "aws_internet_gateway" "default" {
+resource "aws_internet_gateway" "testvpc01" {
   vpc_id = aws_vpc.testvpc01.id
   tags = {
     Name = "${var.vpc_name}-IGW"
